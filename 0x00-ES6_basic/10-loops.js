d74f61191aaca2a902a68b147bbba72124f06787
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2022-12-19 20:41:21 +0100

    ES6 Basics
    tasks 9 & 10 checks

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,11 @@
+/* eslint no-param-reassign: "error" */
+
+export default function appendToEachArrayValue(array, appendString) {
+  let i = 0;
+  for (const idx of array) {
+    array[i] = appendString + idx;
+    i += 1;
+  }
+
+  return array;
+}
