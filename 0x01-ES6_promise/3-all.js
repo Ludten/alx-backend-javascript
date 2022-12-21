7110e60c50c75bc2cdac8aae3c8cdec2ebb88705
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2022-12-21 01:58:39 +0100

    ES6 Promise
    task 3; err handling

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,18 +1,22 @@
 import { createUser, uploadPhoto } from './utils';
 
 export default function handleProfileSignup() {
-  let str;
+  try {
+    let str;
 
-  uploadPhoto()
-    .then((item) => {
-      str = item.body;
-    })
-    .catch(() => console.log('Signup system offline'));
+    uploadPhoto()
+      .then((item) => {
+        str = item.body;
+      })
+      .catch(() => { throw new Error('Signup system offline'); });
 
-  createUser()
-    .then((item) => {
-      str = `${str} ${item.firstName} ${item.lastName}`;
-    })
-    .catch(() => console.log('Signup system offline'))
-    .finally(() => console.log(str));
+    createUser()
+      .then((item) => {
+        str = `${str} ${item.firstName} ${item.lastName}`;
+      })
+      .catch(() => { throw new Error('Signup system offline'); })
+      .finally(() => console.log(str));
+  } catch (err) {
+    console.log('Signup system offline');
+  }
 }
