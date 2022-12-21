553722dcd51dffd15c277618632c475596d72f2f
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2022-12-21 02:11:12 +0100

    ES6 Promise
    task 3; added return, is this a running theme

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { createUser, uploadPhoto } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((item) => {
       console.log(`${item[0].body} ${item[1].firstName} ${item[1].lastName}`);
     })
