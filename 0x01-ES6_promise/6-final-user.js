c6f0310c3bbc1bb566e087c17014381dc9857c6d
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2022-12-21 02:53:15 +0100

    ES6 Promise
    task 5 check; string rep

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,6 +2,14 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
+  const arr = [];
+
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results);
+    .then((results) => {
+      results.forEach((result) => arr.push({
+        status: result.status,
+        value: (result.status === 'fulfilled' ? result.value : String(result.reason)),
+      }));
+      return arr;
+    });
 }
