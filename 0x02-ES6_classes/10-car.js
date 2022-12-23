cd6c9e8f04d9f38c2c32cbaeb2243974ddf3f995
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2022-12-23 02:28:18 +0100

    ES6 Classes
    task 10; species, reading gave clues but didn't help

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,61 +1,17 @@
 export default class Airport {
   constructor(brand, motor, color) {
-    if (typeof (brand) !== 'string') {
-      throw new TypeError('Brand must be a string');
-    } else {
-      this._brand = brand;
-    }
-
-    if (typeof (motor) !== 'string') {
-      throw new TypeError('Motor must be a string');
-    } else {
-      this._motor = motor;
-    }
-
-    if (typeof (color) !== 'string') {
-      throw new TypeError('Color must be a string');
-    } else {
-      this._color = color;
-    }
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
-  get brand() {
-    return this._brand;
-  }
-
-  set brand(val) {
-    if (typeof (val) !== 'string') {
-      throw new TypeError('Brand must be a string');
-    } else {
-      this._brand = val;
-    }
-  }
-
-  get motor() {
-    return this._motor;
-  }
-
-  set motor(val) {
-    if (typeof (val) !== 'string') {
-      throw new TypeError('Motor must be a string');
-    } else {
-      this._motor = val;
-    }
-  }
-
-  get color() {
-    return this._color;
-  }
-
-  set color(val) {
-    if (typeof (val) !== 'string') {
-      throw new TypeError('Color must be a string');
-    } else {
-      this._color = val;
-    }
+  static get [Symbol.species]() {
+    return this;
   }
 
   cloneCar() {
-    return Object.create(Object.getPrototypeOf(this));
+    const Species = this.constructor[Symbol.species];
+
+    return new Species();
   }
 }
