6ee7d1948b7ceb0483c7831555606e8e33feedea
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2023-01-04 02:10:15 +0100

    ES6 Data Manipulation
    Chef Mendie

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,9 @@
-export default function CleanSet(set, startString) {
-  if (!(set instanceof Set) || typeof startString !== 'string' || startString !== '') {
-    return '';
+export default function cleanSet(set, startString) {
+  if ((set instanceof Set) && (typeof startString === 'string')) {
+    let array = Array.from(set);
+    const len = startString.length;
+    array = array.filter((a) => (typeof a === 'string') && (a[0] === startString[0]) && (a.substring(0, len) === startString));
+    return array.map((s) => s.substring(len)).join('-');
   }
-
-  let array = Array.from(set);
-  const len = startString.length;
-  array = array.filter((a) => (a[0] === startString[0]) && (a.substr(0, len) === startString));
-  return array.map((s) => s.substring(len)).join('-');
+  return '';
 }
