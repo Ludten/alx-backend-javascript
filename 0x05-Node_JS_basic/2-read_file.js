4d9a15d32a5aa2ef9045e7f2128f565425dc1f17
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2023-02-21 12:46:04 +0100

    NodeJS Basics
    Mendie sync

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,37 +1,30 @@
 const fs = require('fs');
-const readline = require('readline');
 
 function countStudents(path) {
-  const stream = fs.createReadStream(path);
-  stream.on('error', (err) => {
-    if (err) {
-      throw new Error('Cannot load the database');
-    }
-  });
-  const reader = readline.createInterface({ input: stream });
+  let db;
+  try {
+    db = fs.readFileSync(path, { encoding: 'utf8', flag: 'r' });
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
 
-  const data = [];
+  const data = db.split('\n');
   const cslist = [];
   const swlist = [];
 
-  reader.on('line', (row) => {
-    data.push(row.split(','));
-  });
-
-  reader.on('close', () => {
-    data.forEach((elem) => {
-      if (elem !== [] && elem !== null) {
-        if (elem[3] === 'CS') {
-          cslist.push(elem[0]);
-        } else if (elem[3] === 'SWE') {
-          swlist.push(elem[0]);
-        }
+  data.forEach((stud) => {
+    const elem = stud.split(',');
+    if (elem !== [] && elem !== null) {
+      if (elem[3] === 'CS') {
+        cslist.push(elem[0]);
+      } else if (elem[3] === 'SWE') {
+        swlist.push(elem[0]);
       }
-    });
-    console.log(`Number of students: ${cslist.length + swlist.length}`);
-    console.log(`Number of students in CS: ${cslist.length}. ${cslist.join(', ')}`);
-    console.log(`Number of students in SWE: ${swlist.length}. ${swlist.join(', ')}`);
+    }
   });
+  console.log(`Number of students: ${cslist.length + swlist.length}`);
+  console.log(`Number of students in CS: ${cslist.length}. ${cslist.join(', ')}`);
+  console.log(`Number of students in SWE: ${swlist.length}. ${swlist.join(', ')}`);
 }
 
 module.exports = countStudents;
