629eaa395f238f479bdeaa22a26d5d0894b56df4
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2023-02-21 14:22:16 +0100

    NodeJS Basics
    I'd do it all again

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) reject(new Error('Cannot load the database'));
+      else {
+        const db = data.split('\n');
+        const cslist = [];
+        const swlist = [];
+
+        db.forEach((stud) => {
+          const elem = stud.split(',');
+          if (elem !== [] && elem !== null) {
+            if (elem[3] === 'CS') {
+              cslist.push(elem[0]);
+            } else if (elem[3] === 'SWE') {
+              swlist.push(elem[0]);
+            }
+          }
+        });
+        const str = `Number of students: ${cslist.length + swlist.length}\nNumber of students in CS: ${cslist.length}. List: ${cslist.join(', ')}\nNumber of students in SWE: ${swlist.length}. List: ${swlist.join(', ')}`;
+        resolve(str);
+      }
+    });
+  });
+}
+
+const app = express();
+const port = 1245;
+
+app.get('/', (_req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (_req, res) => {
+  res.write('This is the list of our students\n');
+  const file = process.argv.length > 2 ? process.argv[2] : '';
+  await countStudents(file)
+    .then((data) => {
+      res.write(data);
+    })
+    .catch((error) => {
+      res.write(error.message);
+    });
+    res.end();
+});
+
+app.listen(port);
